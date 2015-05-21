3a7bbc4d535df3a7c479299539c06a92dfb4f270
guywatson <guy.watson@internetrix.com.au>
2015-05-21 16:28:50 +1000

    blacklist

--- code/extensions/PageExtension.php
+++ code/extensions/PageExtension.php
@@ -1,36 +1,35 @@
 <?php
 class PageExtension extends DataExtension {
+	
 	private static $db = array(
-			'ShowListingImageOnPage'	=> 'Int',
-			'ListingSummary'			=> 'HTMLText'
+		'ShowListingImageOnPage'	=> 'Int',
+		'ListingSummary'			=> 'HTMLText'
 	);
 	
 	private static $has_one = array(
-			'ListingImage'	=> 'Image'
+		'ListingImage'	=> 'Image'
 	);
 	
 	public function updateCMSFields(FieldList $fields){
 
-		$targetclasses = Config::inst()->get('ListingSummary', 'targetclasses');
+		$blacklist = Config::inst()->get('ListingSummary', 'blacklist');
 	
 		/***************************************Listing Fields******************************/
-		if($targetclasses && in_array($this->owner->ClassName, $targetclasses)){
+		if( empty($blacklist) || (!empty($blacklist) && !in_array($this->owner->ClassName, $blacklist)) ) {
 			$fields->addFieldToTab('Root.Main', ToggleCompositeField::create('ListingSummaryToggle', 'Listing Summary',
-					array(
-							UploadField::create('ListingImage', 'Listing Image')
-							->addExtraClass('withmargin'),
-							CheckboxField::create('ShowListingImageOnPage', 'Show the listing image on the page '),
-							HtmlEditorField::create('ListingSummary', 'Listing Summary')
-							->setRows(6)
-							->setRightTitle('A listing/category page will source the summary from here.')
-							->addExtraClass('withmargin'),
-					)
+				array(
+					UploadField::create('ListingImage', 'Listing Image')
+						->addExtraClass('withmargin'),
+					CheckboxField::create('ShowListingImageOnPage', 'Show the listing image on the page '),
+					HtmlEditorField::create('ListingSummary', 'Listing Summary')
+						->setRows(6)
+						->setRightTitle('A listing/category page will source the summary from here.')
+						->addExtraClass('withmargin'),
+				)
 			), 'Content');
 		}else{
 			$fields->addFieldToTab('Root.Main', HiddenField::create('ListingSummaryToggle'),'Content'); //just a place holder so other elements are added to the correct spot
 		}
 		/***********************************************************************************/
-
-		
 	}
 }
