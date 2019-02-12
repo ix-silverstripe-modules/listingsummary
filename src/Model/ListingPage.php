008a1a526faaa0a276fab2672bfea90ea3f2d1b3
Drew Carrette <drew.carrette@internetrix.com.au>
2019-02-13 09:33:25 +1100

    Split page and controller

--- src/Model/ListingPage.php
+++ src/Model/ListingPage.php
@@ -6,19 +6,17 @@
 namespace Internetrix\ListingSummary\Model;
 
 use Page;
-use PageController;
 use SilverStripe\Forms\NumericField;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\OptionsetField;
 use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\View\Requirements;
-use SilverStripe\ORM\PaginatedList;
 use UncleCheese\DisplayLogic\Forms\Wrapper;
 use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 use Internetrix\GridFieldExtras\GridFieldConfig_ManySortableRecordEditor;
+use Internetrix\ListingSummary\DataObjects\ListItem;
 
-class ListingPage extends Page {
-	
+class ListingPage extends Page
+{
 	private static $icon = 'vendor/internetrix/silverstripe-listingsummary/images/icons/listingpage';
 	private static $description = 'Lists Children pages or other items on a page';
 	
@@ -37,8 +35,8 @@ class ListingPage extends Page {
 		'PaginationLimit' => 10
 	];
 	
-	public function getCMSFields(){
-		
+	public function getCMSFields()
+    {
 		$fields = parent::getCMSFields();
 		
 		$fields->insertAfter(NumericField::create('PaginationLimit', 'Pagination Limit'), 'MenuTitle');
@@ -49,13 +47,11 @@ class ListingPage extends Page {
 		
 		$fields->addFieldToTab('Root.List', OptionsetField::create('ListSource', 'List Source' ,$this->dbObject('ListSource')->enumValues()));
 
-
-        if(class_exists('GridFieldConfig_ManySortableRecordEditor')) {
+        if (class_exists('GridFieldConfig_ManySortableRecordEditor')) {
             $listConfig = GridFieldConfig_ManySortableRecordEditor::create(30);
         } else {
             $listConfig = GridFieldConfig_RecordEditor::create(30);
         }
-
 
 		$fields->addFieldToTab('Root.List', Wrapper::create(GridField::create( 'ListItems','List Items', $this->ListItems(), $listConfig ))
 			->displayIf("ListSource")->isEqualTo("Custom")->end());
@@ -64,79 +60,4 @@ class ListingPage extends Page {
 		
 		return $fields;
 	}
-}
-
-class ListingPage_Controller extends PageController {
-	
-	public function init() {
-		parent::init();
-		
-		Requirements::javascript('listingsummary/javascript/listingpage.js');
-	}
-	
-	public function getOffset() {
-		if(!isset($_REQUEST['start'])) {
-			$_REQUEST['start'] = 0;
-		}
-		return $_REQUEST['start'];
-	}
-	
-	public function SortSelected($sort){
-		$request 	= $this->getRequest();
-		$sortby 	= $request->getVar('sortby');
-		if(!$sortby){
-			$sortby = 'default';
-		}
-		return $sort == $sortby;
-	}
-	
-	public function getSortFilter() {
-		$request 	= $this->getRequest();
-		$sortby 	= $request->getVar('sortby');
-		$sort 		= null;
-		if($sortby){
-			switch ($sortby){
-				case 'asc':
-					$sort = '"Title" ASC';
-					break;
-				case 'desc':
-					$sort = '"Title" DESC';
-					break;
-				case 'newest':
-					$sort = '"Created" DESC';
-					break;
-				case 'oldest':
-					$sort = '"Created" ASC';
-					break;
-				default:
-					break;
-			}
-		}
-	
-		return $sort;
-	}
-	
-	public function ListChildren($overridePagination = null){
-	
-		if($overridePagination){
-			$this->PaginationLimit = $overridePagination;
-		}
-		
-		if(!$this->PaginationLimit){
-			$this->PaginationLimit = 10;
-		}
-	
-		if($this->ListSource == 'Children'){
-			$children = $this->Children();
-		}else{
-			$children = $this->ListItems();
-		}
-	
-		if($sort = $this->getSortFilter()){
-			$children = $children->sort($sort);
-		}
-	
-		return PaginatedList::create($children, $this->request)->setPageLength($this->PaginationLimit);
-	}
-	
 }
---
+++ src/Model/ListingPageController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Internetrix\ListingSummary\Model;
+
+use PageController;
+use SilverStripe\View\Requirements;
+use SilverStripe\ORM\PaginatedList;
+
+class ListingPageController extends PageController
+{
+    public function init()
+    {
+        parent::init();
+
+        Requirements::javascript('listingsummary/javascript/listingpage.js');
+    }
+
+    public function getOffset()
+    {
+        if (!isset($_REQUEST['start'])) {
+            $_REQUEST['start'] = 0;
+        }
+        return $_REQUEST['start'];
+    }
+
+    public function SortSelected($sort)
+    {
+        $request 	= $this->getRequest();
+        $sortby 	= $request->getVar('sortby');
+        if(!$sortby){
+            $sortby = 'default';
+        }
+        return $sort == $sortby;
+    }
+
+    public function getSortFilter()
+    {
+        $request 	= $this->getRequest();
+        $sortby 	= $request->getVar('sortby');
+        $sort 		= null;
+        if ($sortby) {
+            switch ($sortby){
+                case 'asc':
+                    $sort = '"Title" ASC';
+                    break;
+                case 'desc':
+                    $sort = '"Title" DESC';
+                    break;
+                case 'newest':
+                    $sort = '"Created" DESC';
+                    break;
+                case 'oldest':
+                    $sort = '"Created" ASC';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return $sort;
+    }
+
+    public function ListChildren($overridePagination = null)
+    {
+        if ($overridePagination) {
+            $this->PaginationLimit = $overridePagination;
+        }
+
+        if (!$this->PaginationLimit) {
+            $this->PaginationLimit = 10;
+        }
+
+        if ($this->ListSource == 'Children') {
+            $children = $this->Children();
+        } else {
+            $children = $this->ListItems();
+        }
+
+        if ($sort = $this->getSortFilter()) {
+            $children = $children->sort($sort);
+        }
+
+        return PaginatedList::create($children, $this->request)->setPageLength($this->PaginationLimit);
+    }
+
+}
